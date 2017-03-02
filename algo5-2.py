21b74c8368b3947b29bd48e6de9d3e82674cc777
Ashna Guliani <ashna.guliani@colorado.edu>
2017-03-02 15:55:53 -0700

    huffman works, heap implementation needs work

--- algo5-2.py
+++ algo5-2.py
@@ -10,10 +10,15 @@ class MinHeap:
     heap = []
  
     def __init__(self, freq_symbol_list):
+        # The class takes an input of a list of lists.  Each sub-list is of size 2.  The
+        # first index of the sub-list is the frequency of the symbol.  The second index
+        # is the symbol itself.
         for i in range(0, len(freq_symbol_list)):
             self.insert_element(freq_symbol_list[i])
  
     def get_children_index(self, index):
+        # This function returns the index of the children of the given node.  If a child
+        # does not exist, -1 is returned for the respective index.
         if (2*index) + 1 >= len(self.heap):
             left_index = -1
             right_index = -1
@@ -30,18 +35,26 @@ class MinHeap:
         return self.heap[(index - 1)/2]
  
     def get_parent_index(self, index):
+        # Returns parent's index        
         return int((index - 1)/2)
  
     def insert_element(self, freq_symbol):
+        # This inserts a new element into the heap. It takes input as a list of size 2.
+        # The first index is the frequency and the second is the symbol.  After it is
+        # added, it ensures that it follows minimum properties.
         new_index = len(self.heap)
         self.heap.append(freq_symbol)
         parent_index = self.get_parent_index(new_index)
  
         while self.heap[parent_index][0] >= freq_symbol[0] and parent_index >= 0:
+            # This is done to randomly break ties.
             if self.heap[parent_index][0] == freq_symbol[0] and random.random() > .5:
                 break
  
             else:
+                # This is where the actual restructuring of the heap takes place.  It works
+                # by creating a temporary variable to store the data of one of the indices
+                # to be swapped then moving the values to the new locations.
                 temp = self.heap[parent_index]
                 self.heap[parent_index] = self.heap[new_index]
                 self.heap[new_index] = temp
@@ -50,6 +63,10 @@ class MinHeap:
                 parent_index = self.get_parent_index(new_index)
  
     def pop_min(self):
+        # This function removes the smallest value in the heap and returns a list of length 2.
+        # The list contains the frequency of character in the first index and the symbol in the
+        # next. After the value is removed, the heap is restructured to maintain min-heap
+        # properties.
         minimum = self.heap[0]
  
         self.heap[0] = self.heap[len(self.heap) - 1]
@@ -57,6 +74,9 @@ class MinHeap:
         current_index = 0
  
         left_index, right_index = self.get_children_index(current_index)
+        # The following is done to ensure that if there is no child, the logic of the loop
+        # will continue to function.  Casting "inf" to a float creates the largest possible
+        # value which ensures that this will never be used in the restructuring of the heap.
  
         if left_index != -1:
             left_child = self.heap[left_index]
@@ -82,6 +102,9 @@ class MinHeap:
                 break
  
             else:
+                # This is where the actual restructuring of the heap takes place.  It works
+                # by creating a temporary variable to store the data of one of the indices
+                # to be swapped then moving the values to the new locations.
                 temp = self.heap[current_index]
                 self.heap[current_index] = self.heap[small_index]
                 self.heap[small_index] = temp
@@ -107,6 +130,7 @@ class MinHeap:
 
 
 class Node(object):
+    #Node structure just keeps track of left & right children 
     right_child = None
     left_child = None
     total_weight = None
@@ -132,6 +156,7 @@ class Node(object):
 
 
 def createTree(array, parent):
+    #simple function to take in an array and the parent and return a full tree structure
     left_parent = array[parent.left()]
     right_parent = array[parent.right()]
     parent.create_left = left_parent
@@ -147,6 +172,7 @@ def createTree(array, parent):
 
 
 def createDict(S, parent, encode_string, T):
+    #simple function to take in S, T, and a parent and return the dictionary T
     left = parent.left
     right = parent.right
 
@@ -160,6 +186,10 @@ def createDict(S, parent, encode_string, T):
 
  
 def string2freq(x):
+    #takes as input an ASCII string x and returns (a) a vector S, which
+    #contains the n unique symbols of x in lexicographic order, and (b) a vector 
+    #f containing the frequencies of those symbols, in the same order
+
     total_freq = [0 for _ in range(90)]
     freq = []
     symbols = []
@@ -175,6 +205,8 @@ def string2freq(x):
 
 
 def huffmanEncode(S, f):
+    #takes as input the vectors S and f, and returns a dictionary T, that represents the codebook
+
     length = len(f)
     H = MinHeap([list(x) for x in zip(S, f)])
     
@@ -199,7 +231,10 @@ def huffmanEncode(S, f):
     return T
 
 
-def encodeString(x, T): #takes an input ASCII string x and a codebook T, and returns a string y, which is the binary encoding of x using T:
+def encodeString(x, T): 
+    #takes an input ASCII string x and a codebook T, and returns a string y, 
+    #which is the binary encoding of x using T:
+    
     y = ""
     for i in range(0, len(x)):
         y = y + T[x[i]]
